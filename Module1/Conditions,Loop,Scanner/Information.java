f177c17351c11032f4aeb27e5ec208c73bbee8a3
dhyeychangani9 <110374250+dhyeychangani9@users.noreply.github.com>
2022-08-04 16:33:52 +0530

    Add files via upload

---
+++ Module1/Conditions,Loop,Scanner/Information.java
@@ -0,0 +1,12 @@
+package com_18;
+
+public class Information 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("My name is Dhyey");
+		System.out.println("My birthdate is 02\01\2003");
+		System.out.println("MY age is 20");
+		System.out.println("I am from Rajkot");
+	}
+}
